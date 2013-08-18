c11f6f9d8d01daead924b3b875834b248493585a
Chris Breiding <chrisbreiding@gmail.com>
2013-08-17 20:03:04 -0400

    cleanup

--- public/js/services/vent.js
+++
@@ -1,7 +0,0 @@
-(function() {
-
-  define(['backbone', 'underscore'], function(Backbone, _) {
-    return _.extend({}, Backbone.Events);
-  });
-
-}).call(this);
--- public/templates/subscription-search.hb.js
+++
@@ -1,9 +0,0 @@
-define(['handlebars'], function(Handlebars) {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-return templates['subscription-search.hb'] = template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  
-
-
-  return "<form id=\"sub-search-form\" class=\"form-inline\">\n    <input type=\"text\" id=\"sub-query\">\n    <button class=\"btn btn-primary\" id=\"search-subs\">\n      <i class=\"icon-search\"></i>\n    </button>\n</form>\n";});
-});
