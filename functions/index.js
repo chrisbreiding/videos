a5c6a461635086c4178e9d2ff582740e4af05e40
Chris Breiding <chrisbreiding@gmail.com>
2026-07-15 08:51:07 -0400

    fix function name

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin')
-const { pruneWatchVideos } = require('./scheduled/prune-watched-videos')
+const { pruneWatchedVideos } = require('./scheduled/prune-watched-videos')
 const { runMigrations } = require('./migrations/run-migrations')
 
 admin.initializeApp()
 
 const db = admin.firestore()
 
-exports.pruneWatchVideos = pruneWatchVideos(admin, db)
+exports.pruneWatchVideos = pruneWatchedVideos(admin, db)
 exports.runMigrations = runMigrations(admin, db)
