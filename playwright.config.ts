06e55a3d61a8e11315df4da756bbcb4bd4adbd65
Chris Breiding <chrisbreiding@gmail.com>
2026-07-19 19:42:23 -0400

    parallelize ci tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,9 +10,8 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI */
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  /* Blob reports from each CI shard are merged into a single HTML report */
+  reporter: process.env.CI ? 'blob' : 'html',
   use: {
     baseURL: 'http://localhost:8001',
     trace: 'on-first-retry',
